cbfc17bc8aa17420cc212e422df1a60eb26a141b
Francisco García Macías <garmacfco@gmail.com>
2024-07-26 15:15:55 +0200

    26/07
    - Login corregido
    - Página registro creada

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:kartf1/pages/intro_page.dart';
@@ -25,16 +27,29 @@ class _LoginPageState extends State<LoginPage>{
   void login(String user, password) async{
 
   try{
-    Response response = await post(Uri.parse('https://pacou.pythonanywhere.com/login'),
+
+    Response response = await post(
+      Uri.parse('https://pacou.pythonanywhere.com/login'),
       body: {
         'username': user,
         'password': password,
       }
     );
+
     if(response.statusCode == 200){
+      var data = jsonDecode(response.body.toString());
       print('account logged');
+      // print(data);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => IntroPage()));
     }else{
       print('failed');
+      showDialog(
+      context: context, 
+      builder: (context) => AlertDialog(
+        title: Text('Something went wrong...'),
+        content: Text('Check your credentials'),
+      ),
+    );
     }
   }catch(e){
     print(e.toString());
@@ -71,6 +86,7 @@ class _LoginPageState extends State<LoginPage>{
 
 
             TextField(
+              obscureText: true,
               controller: pwdController,
               decoration: InputDecoration(
                 hintText: 'password',
@@ -83,7 +99,7 @@ class _LoginPageState extends State<LoginPage>{
             GestureDetector(
               onTap: () {
                 login(userController.text.toString(), pwdController.text.toString());
-                Navigator.push(context, MaterialPageRoute(builder: (context) => IntroPage())); 
+                // Navigator.push(context, MaterialPageRoute(builder: (context) => IntroPage())); 
               },
               child: Container(
                 height: 60,
@@ -102,11 +118,104 @@ class _LoginPageState extends State<LoginPage>{
                   ),
                 ),
               ),
-            )
+            ),
+            ElevatedButton(
+              child: const Text('Open route'),
+              onPressed: () {
+                Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Register()),
+              );
+}
+            ),
           ],
         ),
       ),
     );
   }
   
+}
+
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+       body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: Colors.grey[300],
+  //     body: Padding(
+  //       padding: const EdgeInsets.all(8),
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         crossAxisAlignment: CrossAxisAlignment.center,
+  //         children: [
+  //           Icon(
+  //             Icons.lock,
+  //             size: 100,
+  //           ),
+
+  //           const SizedBox(height: 50),
+
+  //           TextField(
+  //             decoration: InputDecoration(
+  //               hintText: 'username',
+  //               border: OutlineInputBorder(
+
+  //               ),
+  //             ),
+  //           ),
+
+  //           const SizedBox(height: 20,),
+
+
+  //           TextField(
+  //             obscureText: true,
+  //             decoration: InputDecoration(
+  //               hintText: 'password',
+  //               border: OutlineInputBorder(
+
+  //               ),
+  //             ),
+  //           ),
+  //           const SizedBox(height: 20,),
+  //           GestureDetector(
+  //             onTap: () {
+  //               login(userController.text.toString(), pwdController.text.toString());
+  //               Navigator.push(context, MaterialPageRoute(builder: (context) => IntroPage())); 
+  //             },
+  //             child: Container(
+  //               height: 60,
+  //               width: 300,
+  //               decoration: BoxDecoration(
+  //                 color: Colors.black,
+  //                 borderRadius: BorderRadius.circular(30),
+  //               ),
+  //               child: const Center(
+  //                 child: Text(
+  //                   'Sign Up', 
+  //                   style: TextStyle(
+  //                     color: Colors.white,
+  //                     fontSize: 18,
+  //                   ),
+  //                 ),
+  //               ),
+  //             ),
+  //           )
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
